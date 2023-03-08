db50051db9a122fd82735f09fc9b60d2ea4196be
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-03-07 23:58:48 -0600

    Operadores en "php", suma, resta, multiplicación y división

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+  $num1 = 20;
+  $num2 = 10;
 
+  // Sumar
+  echo $num1 + $num2;
+  echo '<br>';
 
+  // Restar
+  echo $num1 - $num2;
+  echo '<br>';
+
+  // Multiplicar
+  echo $num1 * $num2;
+  echo '<br>';
+
+  // Dividir
+  echo $num1 / $num2;
+  echo '<br>';
+
+  // Multiplicar cierta cantidad de veces
+  echo 2 ** 3; // Sería 2 * 2 = 4 * 2 = 8 
 
 include 'includes/footer.php';
+?>
