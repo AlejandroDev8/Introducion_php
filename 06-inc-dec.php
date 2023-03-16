947d0b43f9bdfbd5984c6c52201f366a40ee787d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-03-16 01:46:04 -0600

    Incrementos y Decrementos

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+  $num1 = 30;
+  $num1++;
+  echo $num1;
 
+  // Si quieres incrementar en el "echo" tendrías que ponerlo así
+  // echo ++$num1;
+  echo '<br>';
+
+  $num2 = 30;
+  $num2--;
+
+  // Si quieres decrementar en el "echo" tendrías que ponerlo así
+  // echo --$num2;
+  echo $num2;
 
 
 include 'includes/footer.php';
+?>
