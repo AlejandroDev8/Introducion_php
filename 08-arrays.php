bcf5b66029b2267279298497e93ceef7418de520
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-03-28 23:15:48 -0600

    Arrays en php

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,43 @@
 <?php include 'includes/header.php';
 
+// Arrays in PHP
+
+$carrito = ['Tablet', 'Television', 'Computadora'];
+
+// Util para ver los contenidos de un array
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+// Acceder a un elemento del array
+echo $carrito[1];
+
+// Agregar elemento al indice 3 del al array
+$carrito[3] = 'Audífonos';
+
+// Agregar elemento al final del al array
+
+array_push($carrito, 'Teclado');
 
 
+// Agregar elemento al inicio del al array
+
+array_unshift($carrito, 'Celular');
+
+// Agregar elementos al array	
+
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+
+$clientes = array('Cliente 1', 'Cliente 2', 'Cliente 3');
+
+echo '<pre>';
+var_dump($clientes);
+echo '</pre>';
+
+echo $clientes[1];
 
 include 'includes/footer.php';
+?>
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+// Arrays asociativos en PHP
+
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium',
+        'disponible' => 500
+    ]
+];
+
+// Util para ver los contenidos de un array
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
+
+// Acceder a un elemento del array
+echo $cliente['nombre'];
+echo $cliente['informacion']['disponible'];
 
 
+// Agregar un elemento al array Si se agrega una propiedad que no existe, se crea, pero si se agrega una propiedad que ya existe, se sobrescribe
+
+$cliente['codigo'] = 12345;
+
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
 
 include 'includes/footer.php';
+?>
